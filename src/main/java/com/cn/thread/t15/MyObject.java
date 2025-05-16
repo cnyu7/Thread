d3e806bd9030ace13794c9fad54e01e1a4dc7e55
cn <1921337965@qq.com>
2025-05-16 22:04:08 +0800

    synchronized方法调用

---
+++ src/main/java/com/cn/thread/t15/MyObject.java
@@ -0,0 +1,26 @@
+package com.cn.thread.t15;
+
+public class MyObject {
+  synchronized   public void methodA() {
+        try {
+            System.out.println("begin methodA threadName="
+                    + Thread.currentThread().getName());
+            Thread.sleep(5000);
+            System.out.println("A end endTime=" + System.currentTimeMillis());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void methodB() {
+        try {
+            System.out.println("begin methodB threadName=" + Thread.currentThread().getName() +  " begin time="
+                    + System.currentTimeMillis());
+            Thread.sleep(5000);
+            System.out.println("B end");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/cn/thread/t15/ThreadA.java
@@ -0,0 +1,17 @@
+package com.cn.thread.t15;
+
+public class ThreadA extends Thread {
+
+    private MyObject object;
+
+    public ThreadA(MyObject object) {
+        super();
+        this.object = object;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        object.methodA();
+    }
+}
