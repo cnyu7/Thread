e02006d35ef05026e71c8e35acc2bbdaff39ee83
cn <1921337965@qq.com>
2025-05-17 10:52:45 +0800

    出现异常,锁自动释放

---
+++ src/main/java/com/cn/thread/t19/ThreadA.java
@@ -0,0 +1,19 @@
+package com.cn.thread.t19;
+
+import com.cn.thread.t15.MyObject;
+
+public class ThreadA extends Thread {
+
+    private Service service;
+
+    public ThreadA(Service service) {
+        super();
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        service.testMethod();
+    }
+}
---
+++ src/main/java/com/cn/thread/t19/ThreadB.java
@@ -0,0 +1,17 @@
+package com.cn.thread.t19;
+
+public class ThreadB extends Thread {
+
+    private Service service;
+
+    public ThreadB(Service service) {
+        super();
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        service.testMethod();
+    }
+}
