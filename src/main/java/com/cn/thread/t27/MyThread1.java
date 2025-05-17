e3663a55ee07692fbee94e9806c41fd17611b754
cn <1921337965@qq.com>
2025-05-17 14:50:43 +0800

    synchronized(this)代码块是锁定当前对象的

---
+++ src/main/java/com/cn/thread/t27/MyThread1.java
@@ -0,0 +1,17 @@
+package com.cn.thread.t27;
+
+
+
+public class MyThread1 extends Thread{
+
+    private Service service;
+    public MyThread1 (Service service) {
+        super();
+        this.service = service;
+    }
+        @Override
+        public void run () {
+            super.run();
+            service.setUsernamePassword("a","aa");
+        }
+}
---
+++ src/main/java/com/cn/thread/t28/MyThread2.java
@@ -0,0 +1,17 @@
+package com.cn.thread.t28;
+
+
+
+public class MyThread2 extends Thread{
+
+    private Service service;
+    public MyThread2(Service service) {
+        super();
+        this.service = service;
+    }
+        @Override
+        public void run () {
+            super.run();
+            service.b();
+        }
+}
