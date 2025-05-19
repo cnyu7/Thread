8bb54ffbfb82f4edc08198d61279f7d6f34c6df9
cn <1921337965@qq.com>
2025-05-19 22:50:03 +0800

    InheritableThreadLocal继承值,修改值

---
+++ src/main/java/com/cn/thread/threadlocal/t5/ThreadA.java
@@ -0,0 +1,17 @@
+package com.cn.thread.threadlocal.t5;
+
+
+
+public class ThreadA extends Thread {
+    public void run() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("在ThreadA get Value=" + Tools.t1.get());
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+        }
+    }
+}
---
+++ src/main/java/com/cn/thread/threadlocal/t5/Tools.java
@@ -0,0 +1,6 @@
+package com.cn.thread.threadlocal.t5;
+
+
+public class Tools {
+    public static InheritableThreadLocalExt t1 = new InheritableThreadLocalExt();
+}
