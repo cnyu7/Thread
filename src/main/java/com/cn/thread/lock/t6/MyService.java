eeb82c599a54b60124fd8b50edf4d2fe446167ef
liucongneng <513361602@qq.com>
2025-05-20 13:26:19 +0800

    多个condition唤醒signalAll

---
+++ src/main/java/com/cn/thread/lock/t6/MyService.java
@@ -0,0 +1,62 @@
+package com.cn.thread.lock.t6;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MyService {
+    private Lock lock = new ReentrantLock();
+    public Condition conditionA = lock.newCondition();
+    public Condition conditionB = lock.newCondition();
+
+    public void awaitA() {
+        try {
+            lock.lock();
+            System.out.println("begin awaitA时间为" + System.currentTimeMillis()
+                    + " ThreadName=" + Thread.currentThread().getName());
+            conditionA.await();
+            System.out.println("  end awaitA时间为" + System.currentTimeMillis()
+                    + " ThreadName=" + Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void awaitB() {
+        try {
+            lock.lock();
+            System.out.println("begin awaitB时间为" + System.currentTimeMillis()
+                    + " ThreadName=" + Thread.currentThread().getName());
+            conditionB.await();
+            System.out.println("  end awaitB时间为" + System.currentTimeMillis()
+                    + " ThreadName=" + Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void signalAllA() {
+        try {
+            lock.lock();
+            System.out.println("  signalAll 时间为" + System.currentTimeMillis()
+                    + " ThreadName=" + Thread.currentThread().getName());
+            conditionA.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+    public void signalAllB() {
+        try {
+            lock.lock();
+            System.out.println("  signalAll 时间为" + System.currentTimeMillis()
+                    + " ThreadName=" + Thread.currentThread().getName());
+            conditionB.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
